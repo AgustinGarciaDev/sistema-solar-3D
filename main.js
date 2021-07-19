a08cb9713fe2aa29fd31d0919c9783183bf42734
Agustin <agustinchinchilla16@gmail.com>
2021-07-18 23:46:56 -0300

    add moon

--- main.js
+++ main.js
@@ -7,23 +7,63 @@ const scene = new THREE.Scene();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.TorusGeometry(0.5,2,4,4);
+			const geometry = new THREE.TorusGeometry(0.5,2,10,4);
 			const material = new THREE.MeshBasicMaterial( { color: "#10faf4" , wireframe:true} );
 			const torus = new THREE.Mesh( geometry, material );
 			scene.add( torus );
 
 			camera.position.z = 5;
-            
-            const  constrols = new OrbitControls(camera, renderer.domElement)
+
+			 const gridHelper = new THREE.GridHelper(200,50)
+			scene.add(gridHelper) 
+            const  controls = new OrbitControls(camera, renderer.domElement)
+			
+			function addStart() {
+				
+				const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+				const material = new THREE.MeshBasicMaterial({ color: "#FFFFFF" });
+				const star = new THREE.Mesh(geometry, material);
+
+				const [x, y, z] = Array(3)
+					.fill()
+					.map(() => THREE.MathUtils.randFloatSpread(150));
+
+				star.position.set(x, y, z);
+				scene.add(star);
+
+			};
+			Array(200).fill().forEach(addStart)
+			
+			const spaceTexture = new THREE.TextureLoader().load('space-two.jpg')
+			scene.background = spaceTexture
+
+			const marteTexture = new THREE.TextureLoader().load('moon.jpg')
+
+			const marte = new THREE.Mesh(
+				new THREE.SphereGeometry(3, 32, 32),
+				new THREE.MeshBasicMaterial({
+					map: marteTexture ,
+					
+				})
+			)
+
+			scene.add(marte)
+			marte.position.z = 20;
+			marte.position.setX(-5);
 
 			const animate = function () {
 				requestAnimationFrame( animate );
 
 				torus.rotation.x += 0.01;
+                torus.rotation.y += 0.02;
                 torus.rotation.y += 0.01;
-                torus.rotation.y += 0.01;
+  				marte.rotation.x += 0.005;
 
-				renderer.render( scene, camera );
+			/* 	controls.update() */
+
+				renderer.render(scene, camera);
+					
 			};
-
 			animate();
+
+		
